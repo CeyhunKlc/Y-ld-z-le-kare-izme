937ec3eaf144f4c3c6c64d4ed9f81686b003b9c0
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-20 15:01:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Kare_Cizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Kenar Sayısına Eşit yıldız sayısı giriniz:");
+            int yildiz;
+            yildiz = Convert.ToInt16(Console.ReadLine());
+             for (int a = 1; a <= yildiz; a++)
+            {
+
+                for (int b = 1; b <= yildiz; b++)
+                {
+                    if (a == 1 || a == yildiz)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if (b == 1 || b == yildiz)
+                    {
+                        Console.Write("* ");
+                    }
+
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+           
+            }        
+            
+        }
+    }
+}
